6bb5a63132937ce994c1b71b4f8ed6818ee42641
Santiago Hernández Mejía <santiago.hernandez@wolox.co>
2019-06-04 14:51:12 -0500

    Changes requested to API consumption

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,8 @@
-// const controller = require('./controllers/controller');
-const { healthCheck } = require('./controllers/healthCheck');
-const { getAlbums, getPhotos } = require('./controllers/album');
+const { healthCheck } = require('./controllers/healthCheck'),
+  { getAlbumsInventory, getPhotos } = require('./controllers/album');
+
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.get('/albums', getAlbums);
+  app.get('/albums', getAlbumsInventory);
   app.get('/albums/:id/photos', getPhotos);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
