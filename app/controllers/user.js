66ee5d7478c977d439d2e8ff783533347687c475
Santiago Hernández Mejía <santiago.hernandez@wolox.co>
2019-06-14 11:16:17 -0500

    Modifications made to the controller, service and user model.

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,12 +4,12 @@ const errors = require('../errors');
 const logger = require('../logger');
 
 exports.createUser = (req, res, next) => {
-  const { body } = req;
-  if (validations.paramsValidation(body.password, body.email)) {
+  const { password, email } = req.body;
+  if (validations.paramsValidation(password, email)) {
     return next(errors.invalidParameters('The email or password does not meet the conditions.'));
   }
   return validations
-    .passwordEncryption(body)
+    .passwordEncryption(req.body)
     .then(userService.userRegister)
     .then(response => {
       logger.info(`User ${response.firstName} ${response.lastName} created successfully`);
