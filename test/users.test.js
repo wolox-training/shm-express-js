4735ee54de4c9a07e16a90d95aade2aa3ef0d5ce
shernanm <50426028+shernanm@users.noreply.github.com>
2019-06-19 11:10:06 -0500

    Tests documentation (#6)
    Tests and documentation

---
+++ test/users.test.js
@@ -0,0 +1,97 @@
+const request = require('supertest');
+const dictum = require('dictum.js');
+
+const app = require('../app');
+const { User } = require('../app/models');
+
+const controller = request(app);
+
+describe('POST /users', () => {
+  test('Successful test when registering user', () =>
+    controller
+      .post('/users')
+      .send({
+        firstName: 'John',
+        lastName: 'Katzenbach',
+        email: 'john.katz@wolox.co',
+        password: '12345678'
+      })
+      .then(response =>
+        User.findOne({
+          where: { email: 'john.katz@wolox.co' }
+        }).then(({ firstName, lastName, email }) => {
+          expect({ firstName, lastName, email }).toStrictEqual({
+            firstName: 'John',
+            lastName: 'Katzenbach',
+            email: 'john.katz@wolox.co'
+          });
+          dictum.chai(response, 'Successful test creating user');
+        })
+      ));
+
+  test('User creation test when using an email in use', () =>
+    controller
+      .post('/users')
+      .send({
+        firstName: 'Maria Jose',
+        lastName: 'Perez',
+        email: 'jose.perez@wolox.com.ar',
+        password: 'test12345'
+      })
+      .then(() =>
+        controller
+          .post('/users')
+          .send({
+            firstName: 'Jose',
+            lastName: 'Perez',
+            email: 'jose.perez@wolox.com.ar',
+            password: '12345test'
+          })
+          .then(response => {
+            const { message, internal_code } = response.body;
+            expect({ status: response.statusCode, message, internal_code }).toStrictEqual({
+              status: 503,
+              message: 'Error processing request in database.',
+              internal_code: 'database_error'
+            });
+            dictum.chai(response, 'Successful test trying to create an user with an email in use.');
+          })
+      ));
+
+  test('User creation test when the password meets conditions', () =>
+    controller
+      .post('/users')
+      .send({
+        firstName: 'John',
+        lastName: 'Katzenbach',
+        email: 'john.katz@wolox.co',
+        password: '123'
+      })
+      .then(response => {
+        const { message, internal_code } = response.body;
+        expect({ status: response.statusCode, message, internal_code }).toStrictEqual({
+          status: 400,
+          message: 'The email or password does not meet the conditions.',
+          internal_code: 'invalid_parameters_error'
+        });
+        dictum.chai(response, 'Test when the password meets conditions');
+      }));
+
+  test('User creation test when required parameters are not sent', () =>
+    controller
+      .post('/users')
+      .send({
+        lastName: 'Katzenbach',
+        email: 'john.katz@wolox.co',
+        password: '12345678'
+      })
+      .then(response => {
+        const { message, internal_code } = response.body;
+        expect({ status: response.statusCode, message, internal_code }).toStrictEqual({
+          status: 503,
+          message: 'Error processing request in database.',
+          internal_code: 'database_error'
+        });
+        dictum.chai(response, 'Test when required parameters are not sent');
+      }));
+});
